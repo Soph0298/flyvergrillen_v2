a3db51f426267d0b9d549aee2390390664776c0a
jaco7565 <jaco7565@stud.kea.dk>
2021-06-02 16:08:05 +0200

    Rettelser popup på index.html

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
-const Popup = document.querySelector("#popup_container");
-const knap = document.querySelector("#popupknap");
+const Popup = document.querySelector(".popup_container");
+const knap = document.querySelector(".popupknap");
 
 window.addEventListener("load", SidenVises);
 
@@ -11,4 +11,19 @@ function SidenVises() {
 function Clickhandler() {
   console.log("click");
   Popup.classList.add("skjul");
+  window.removeEventListener("load", SidenVises);
 }
+// const Popup = document.querySelector("#popup_container");
+// const knap = document.querySelector("#popupknap");
+
+// window.addEventListener("load", SidenVises);
+
+// function SidenVises() {
+//   console.log("siden vises");
+//   knap.addEventListener("click", Clickhandler);
+// }
+
+// function Clickhandler() {
+//   console.log("click");
+//   Popup.classList.add("skjul");
+// }
