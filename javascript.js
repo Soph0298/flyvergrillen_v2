0a6b0ff5c0e3d6997a04aff3f020ead424f0e1bd
Your Name <Soph0298@stud.kea.dk>
2021-06-03 14:48:10 +0200

    final edit (uden validator)

--- javascript.js
+++ javascript.js
@@ -13,17 +13,3 @@ function Clickhandler() {
   Popup.classList.add("skjul");
   window.removeEventListener("load", SidenVises);
 }
-// const Popup = document.querySelector("#popup_container");
-// const knap = document.querySelector("#popupknap");
-
-// window.addEventListener("load", SidenVises);
-
-// function SidenVises() {
-//   console.log("siden vises");
-//   knap.addEventListener("click", Clickhandler);
-// }
-
-// function Clickhandler() {
-//   console.log("click");
-//   Popup.classList.add("skjul");
-// }
